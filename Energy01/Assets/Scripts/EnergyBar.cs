67d63ea6e802e628db124c82c83608e09624b959
ExternalPC <s.roach0305@gmail.com>
2018-04-15 17:49:47 +0100

    Locked camera to follow. Created energy bar which will move on fill. Remap value logic for energy bar fill vs max / min energy needs fixing .

--- Energy01/Assets/Scripts/EnergyBar.cs
+++ Energy01/Assets/Scripts/EnergyBar.cs
@@ -5,16 +5,40 @@ using UnityEngine.UI;
 
 namespace Energy
 {
+    public static class RemapExtension
+    {
+        public static float Remap(this float value, float from1, float to1, float from2, float to2)
+        {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+    }
     public class EnergyBar : MonoBehaviour
     {
-        public Image energyBar;
-        public float energyAmount;
-        public float fillAmount;
+
+        public Image energybarUI;
+        public bool increaseOverTime;
+        //[Range(0, 10)]
+        public float currentEnergy;
+
+
+        private float maxEnergy;
+        private float minEnergy;
+
+        private void Start()
+        {
+            maxEnergy = 50;
+            minEnergy = 0;
+        }
+
+
         void Update()
         {
-
-            energyBar.fillAmount = fillAmount ;/* += 1.0f / energyAmount * Time.deltaTime;*/
-
+            
+            if (increaseOverTime)
+            {
+                energybarUI.fillAmount = (currentEnergy.Remap(minEnergy, maxEnergy, 0, 100));
+                Debug.Log(currentEnergy.Remap(minEnergy, maxEnergy, 0, 1) + "current fill");
+            }
         }
     }
 }
